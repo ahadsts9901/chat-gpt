306d37dc0a1e800e69c8bc86959ffeab31ac2c99
Muhammad Ahad <120649081+ahadsts9901@users.noreply.github.com>
2023-07-26 07:44:07 -0700

    swal

--- main.js
+++ main.js
@@ -1,20 +1,49 @@
-const chatInput = document.querySelector("#chat-input");
-const sendButton = document.querySelector("#send-btn");
-const chatContainer = document.querySelector(".chat-container");
-const themeButton = document.querySelector("#theme-btn");
-const deleteButton = document.querySelector("#delete-btn");
+// taking api key
+
+async function getUserInput() {
+    const { value: apiKey } = await Swal.fire({
+    //   icon: './assets/logo.webp',
+      title: 'Enter your API key',
+      input: 'text',
+      inputPlaceholder: 'Enter your API key...',
+      footer: `<div class='column'><p>Don't have an API key ?</p><a href="https://platform.openai.com/account/api-keys"> Get an API key</a></div>`,
+      showCancelButton: true,
+      confirmButtonColor: "#00A67E",
+      cancelButtonColor: "#00A67E"
+    });
+
+    // Check if the user provided an API key or clicked Cancel
+    if (apiKey) {
+      // User entered an API key, do something with it
+      console.log('API Key:', apiKey);
+    } else {
+      // User clicked Cancel or closed the dialog
+      console.log('No API key entered.');
+    }
+  }
+
+  // Call the function to display the dialog and get user input
+  getUserInput();
+
+//   code starts
+
+let chatInput = document.querySelector("#chat-input");
+let sendButton = document.querySelector("#send-btn");
+let chatContainer = document.querySelector(".chat-container");
+let themeButton = document.querySelector("#theme-btn");
+let deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "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"; // Paste your API key here
+let API_KEY = "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";
 
-const loadDataFromLocalstorage = () => {
+let loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
-    const themeColor = localStorage.getItem("themeColor");
+    let themeColor = localStorage.getItem("themeColor");
 
     document.body.classList.toggle("light-mode", themeColor === "light_mode");
     themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
 
-    const defaultText = `<div class="default-text">
+    let defaultText =   `<div class="default-text">
                             <h1>ChatGPT Clone</h1>
                             <p>Start a conversation and explore the power of AI.<br> Your chat history will be displayed here.</p>
                         </div>`
@@ -23,20 +52,20 @@ const loadDataFromLocalstorage = () => {
     chatContainer.scrollTo(0, chatContainer.scrollHeight); // Scroll to bottom of the chat container
 }
 
-const createChatElement = (content, className) => {
+let createChatElement = (content, className) => {
     // Create new div and apply chat, specified class and set html content of div
-    const chatDiv = document.createElement("div");
+    let chatDiv = document.createElement("div");
     chatDiv.classList.add("chat", className);
     chatDiv.innerHTML = content;
     return chatDiv; // Return the created chat div
 }
 
-const getChatResponse = async(incomingChatDiv) => {
-    const API_URL = "https://api.openai.com/v1/completions";
-    const pElement = document.createElement("p");
+let getChatResponse = async(incomingChatDiv) => {
+    let API_URL = "https://api.openai.com/v1/completions";
+    let pElement = document.createElement("p");
 
     // Define the properties and data for the API request
-    const requestOptions = {
+    let requestOptions = {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -54,7 +83,7 @@ const getChatResponse = async(incomingChatDiv) => {
 
     // Send POST request to API, get response and set the reponse as paragraph element text
     try {
-        const response = await (await fetch(API_URL, requestOptions)).json();
+        let response = await (await fetch(API_URL, requestOptions)).json();
         pElement.textContent = response.choices[0].text.trim();
     } catch (error) { // Add error class to the paragraph element and set error text
         pElement.classList.add("error");
@@ -68,19 +97,20 @@ const getChatResponse = async(incomingChatDiv) => {
     chatContainer.scrollTo(0, chatContainer.scrollHeight);
 }
 
-const copyResponse = (copyBtn) => {
+let copyResponse = (copyBtn) => {
     // Copy the text content of the response to the clipboard
-    const reponseTextElement = copyBtn.parentElement.querySelector("p");
+    let reponseTextElement = copyBtn.parentElement.querySelector("p");
     navigator.clipboard.writeText(reponseTextElement.textContent);
     copyBtn.textContent = "done";
     setTimeout(() => copyBtn.textContent = "content_copy", 1000);
 }
 
-const showTypingAnimation = () => {
+let showTypingAnimation = () => {
     // Display the typing animation and call the getChatResponse function
-    const html = `<div class="chat-content">
+    let html = `<div class="chat-content">
                     <div class="chat-details">
-                        <img src="images/chatbot.jpg" alt="chatbot-img">
+                        <img src="./assets/logo.webp">
+                        
                         <div class="typing-animation">
                             <div class="typing-dot" style="--delay: 0.2s"></div>
                             <div class="typing-dot" style="--delay: 0.3s"></div>
@@ -90,13 +120,13 @@ const showTypingAnimation = () => {
                     <span onclick="copyResponse(this)" class="material-symbols-rounded">content_copy</span>
                 </div>`;
     // Create an incoming chat div with typing animation and append it to chat container
-    const incomingChatDiv = createChatElement(html, "incoming");
+    let incomingChatDiv = createChatElement(html, "incoming");
     chatContainer.appendChild(incomingChatDiv);
     chatContainer.scrollTo(0, chatContainer.scrollHeight);
     getChatResponse(incomingChatDiv);
 }
 
-const handleOutgoingChat = () => {
+let handleOutgoingChat = () => {
     userText = chatInput.value.trim(); // Get chatInput value and remove extra spaces
     if (!userText) return; // If chatInput is empty return from here
 
@@ -104,7 +134,7 @@ const handleOutgoingChat = () => {
     chatInput.value = "";
     chatInput.style.height = `${initialInputHeight}px`;
 
-    const html = `<div class="chat-content">
+    let html = `<div class="chat-content">
                     <div class="chat-details">
                         <img src="images/user.jpg" alt="user-img">
                         <p>${userText}</p>
@@ -112,8 +142,8 @@ const handleOutgoingChat = () => {
                 </div>`;
 
     // Create an outgoing chat div with user's message and append it to chat container
-    const outgoingChatDiv = createChatElement(html, "outgoing");
-    const defaultTextElement = chatContainer.querySelector(".default-text");
+    let outgoingChatDiv = createChatElement(html, "outgoing");
+    let defaultTextElement = chatContainer.querySelector(".default-text");
     if (defaultTextElement) {
         defaultTextElement.remove();
     }
@@ -137,7 +167,7 @@ themeButton.addEventListener("click", () => {
     themeButton.innerText = document.body.classList.contains("light-mode") ? "dark_mode" : "light_mode";
 });
 
-const initialInputHeight = chatInput.scrollHeight;
+let initialInputHeight = chatInput.scrollHeight;
 
 chatInput.addEventListener("input", () => {
     // Adjust the height of the input field dynamically based on its content
